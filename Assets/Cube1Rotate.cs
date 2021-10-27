2b857f148e8abbd36b71f9231a89a9f60169c18c
Bary Pollack <bpolla01@baker.edu>
2021-10-27 12:45:52 -0700

    2 cubes; C1 moves

---
+++ Assets/Cube1Rotate.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube1Rotate : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    float m_Speed = 5f;
+
+    void Start()
+    {
+        //Fetch the Rigidbody component you attach from your GameObject
+        m_Rigidbody = GetComponent<Rigidbody>();
+        if (m_Rigidbody == null)
+        {
+            print("NULL rigidbody");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
+            m_Rigidbody.velocity = transform.forward * m_Speed;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
+            m_Rigidbody.velocity = -transform.forward * m_Speed;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Rotate the sprite about the Y axis in the positive direction
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * m_Speed, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Rotate the sprite about the Y axis in the negative direction
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * m_Speed, Space.World);
+        }
+    }
+
+
+    /* ------------------
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Cube1Rotate");
+        //transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Y))
+        {
+            print("YYY key");
+        }
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(_speed * Time.deltaTime * direction);
+
+        // Constrain vertical motion
+        transform.position = new Vector3(transform.position.x,
+                                 Mathf.Clamp(transform.position.y, -3.8f, 5.8f));
+
+        // Wrap horizontal motion
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    } */
+
+}
