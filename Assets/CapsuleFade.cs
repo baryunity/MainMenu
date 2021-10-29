022b7b8d01fbe74d5c8c957333af48d519a6a6ec
Bary Pollack <bpolla01@baker.edu>
2021-10-28 20:11:12 -0700

    Capsule changing colors

---
+++ Assets/CapsuleFade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleFade : MonoBehaviour
+{
+    [SerializeField]
+    private float duration = 0.55f;
+
+    private float t = 0f;
+    private Color color1 = Color.red, color2 = Color.blue;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Color color = Color.Lerp(color1, color2, t);
+        t += Time.deltaTime / duration;
+        GetComponent<Renderer>().material.color = color;
+        if (t > duration)
+        {
+            t = 0;
+            color1 = Color.red;
+            color2 = Color.blue;
+        }
+    }
+}
