31e740773ccae1b2ae85a7d6e36c7cdf0433e848
Bary Pollack <bpolla01@baker.edu>
2021-11-25 22:29:40 -0800

    Getting ready to begin again

--- Assets/Scripts/CapsuleFade.cs
+++ Assets/Scripts/CapsuleFade.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+﻿using System.Collections; 
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CapsuleFade : MonoBehaviour
+public class CapsuleFade : MonoBehaviour 
 {
     [SerializeField]
     private float duration = 0.55f;
