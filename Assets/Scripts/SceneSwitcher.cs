2d78d6d3d00c88ecb33e90a8ec48ffd415e8a51a
Bary Pollack <bpolla01@baker.edu>
2021-11-26 00:06:21 -0800

    Both Main Menu buttons functional

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,19 @@
+﻿//using System.Collections;
+//using System.Collections.Generic; 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SwitchToCapsuleScene()
+    {
+        print("SceneSwitcher: switch to capsule");  //FOO
+        SceneManager.LoadScene(1);
+    }
+
+    public void SwitchToCubesScene()
+    {
+        print("SceneSwitcher: switch to cubes");  //FOO
+        SceneManager.LoadScene(2);
+    }
+}
